c3286a314f1ff53130506b2702be01506d26409a
bartoszgamza <bartosz.gamza12@gmail.com>
2018-03-25 15:47:10 +0200

    vuex fixed

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ const router = new VueRouter({
   routes
 })
 
-
 new Vue({
   el: '#app',
   router,
--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -38,7 +38,7 @@ const getters = {
     return state.stocks.map(stock => {
       const record = getters.stocks.find(element => element.id == stock.id)
       return {
-        id: stockId,
+        id: stock.id,
         quantity: stock.quantity,
         name: record.name,
         price: record.price
